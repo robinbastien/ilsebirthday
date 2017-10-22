a15b6d10678b6344c580d1baed17835db4989db8
robinbastien <robin@Robins-Macbook.local>
2017-10-22 00:29:47 -0400

    An initial push to the repository for Ilse Lioness Sweetaroo

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/**
+ * Main.js
+ * @author Robin Bastien <info@robinbastien.com>
+ */
+
+$(document).ready( function() {
+
+  $('.container--content').on('click', function() {
+
+    $('body').addClass('stage-2 is-animating');
+
+  });
+
+});
